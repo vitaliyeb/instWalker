1725f0e524229dc0b9965a4db99209f30f799c41
Vitaliy Burdin <vlvlv.vovov@bk.ru>
2022-02-14 19:44:14 +0500

    functions go to tag

--- script/Utils.js
+++ script/Utils.js
@@ -13,19 +13,53 @@ export default class Utils {
         this.page.click(buttonSelector);
     }
 
-    async goToRandomTag() {
-        const searchSelector = '.XTCLo.d_djL.DljaH';
-        const page = this.page;
 
-        await page.focus(searchSelector);
-        await page.type(searchSelector, '#maroco', {delay: 100});
+    getRandomItem(arr) {
+        return arr[Math.round((arr.length - 1) * Math.random())]
+    }
+
+    async goToRandomTag() {
+        await this.goToTag(this.getRandomItem(tags));
     }
 
     async goToTag(tag) {
+        const searchSelector = '.XTCLo.d_djL.DljaH';
+        const item = '.-qQT3';
+        const page = this.page;
 
+        await page.focus(searchSelector);
+        page.evaluate((tag, searchSelector) => {
+            document.querySelector(searchSelector).value = tag;
+        }, tag, searchSelector);
+        await page.type(searchSelector, ' ', {delay: 200});
+
+        const tagItems = await this.waitFor(async () => {
+            const items =  await page.$$(item);
+            return items.length ? items : false;
+        });
+
+        this.getRandomItem(tagItems).click();
     }
 
-    getRandomTag() {
-        return Math.random() * tags.length;
+    async waitFor(callback, timeAwait = 5000) {
+        const page = this.page;
+        const interval = 100;
+        let timer = timeAwait;
+
+        return await new Promise((resolve, reject) => {
+            const intervalId = setInterval(async () => {
+
+                console.log('tick');
+                if ((timer-=interval) <= 0) {
+                    clearInterval(intervalId);
+                    reject(new Error('error from waitFor'));
+                };
+                const result = await callback();
+                if (result) {
+                    clearInterval(intervalId);
+                    resolve(result);
+                };
+            }, interval)
+        })
     }
 }
